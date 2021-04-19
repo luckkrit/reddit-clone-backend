03d21a1a9d14cbe3508a1017efacdfbeefcc52de
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-19 17:35:52 +0700

    Add AuthService and UserRepository

--- src/main/java/com/k9/redditclonebackend/service/AuthService.java
+++ src/main/java/com/k9/redditclonebackend/service/AuthService.java
@@ -2,6 +2,7 @@ package com.k9.redditclonebackend.service;
 
 import com.k9.redditclonebackend.dto.RegisterRequest;
 import com.k9.redditclonebackend.model.User;
+import com.k9.redditclonebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,11 @@ import java.time.Instant;
 @Service
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
     @Autowired
-    public AuthService(PasswordEncoder passwordEncoder) {
+    public AuthService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     public void signup(RegisterRequest registerRequest){
@@ -23,5 +26,6 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+        userRepository.save(user);
     }
 }
